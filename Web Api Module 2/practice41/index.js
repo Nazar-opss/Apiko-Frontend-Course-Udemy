ec946035a2e88ca111404ed63193b509b6f36bcb
Nazar-opss <nazar4uk132@gmail.com>
2023-10-18 22:26:40 +0300

    complete practice41

--- Web Api Module 2/practice41/index.js
+++ Web Api Module 2/practice41/index.js
@@ -24,9 +24,18 @@
 import API from "./api.js";
 
 let listOfMovies = document.getElementById('listOfMovies')
+let loading = document.querySelector('.loading')
+loading.remove();
+const filmList = await API.fetchPopularMovies();
 
-
-let neew = new API();
-console.log(neew);
-listOfMovies.innerHTML = neew;
-console.log(listOfMovies);
+let renderPopularMovies = (filmList) => {
+    filmList.forEach(movieItem => {
+        const imagePath = "https://image.tmdb.org/t/p/w300";
+        const { poster_path, original_title } = movieItem
+        const moviesElement = document.createElement('li');
+        moviesElement.classList.add('movie');
+        moviesElement.innerHTML = `<img src="${imagePath + poster_path}" alt="#"/><h4>${original_title}</h4>`;
+        listOfMovies.appendChild(moviesElement)
+    });
+}
+renderPopularMovies(filmList);
